6d59bf8abf17517a91643f3e308c7d587e051394
dana1 <danielaraya2191@gmail.com>
2024-08-30 15:58:46 +0300

    introduce auth-service and auth-controller & introduce dependency injection

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
-@Module({})
+@Module({
+    imports: [],
+    controllers: [AuthController],
+    providers: [AuthService]
+})
 export class AuthModule {
 
 }
