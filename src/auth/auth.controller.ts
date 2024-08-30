cbbc418698d81d90c6d28a2c732aff66b8c4da77
dana1 <danielaraya2191@gmail.com>
2024-08-30 16:01:48 +0300

    declare dependency injection between auth-controller and auth-service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
 import { Controller } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Controller({})
 export class AuthController {
+    // constructor(private authService: AuthService) {}
+    authService: AuthService;
+    constructor(authService: AuthService) {
+        this.authService = authService;
+    }
 
 }
